70e94ec0e1ca0c4681d58799b28a1a981739366b
Moritz Kobel <moritz.kobel@itds.ch>
2020-06-25 10:16:56 +0200

    initial commit with minimal project from start.spring.io

---
+++ src/main/java/ch/itds/demo/mvcbindingissue/MvcBindingIssueApplication.java
@@ -0,0 +1,13 @@
+package ch.itds.demo.mvcbindingissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcBindingIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcBindingIssueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/itds/demo/mvcbindingissue/MvcBindingIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.itds.demo.mvcbindingissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcBindingIssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
